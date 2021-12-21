76225903232dbf5751f0ed91f3fcb9657d4ec228
Rhyannon <rhyannonjoy@gmail.com>
2021-12-21 10:58:05 -0700

    Added tag to SelectedBoard.js, needs styling + functionality

--- inspiration-board-frontend/src/App.js
+++ inspiration-board-frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Boards from "./components/Boards";
 import CreateANewBoard from "./components/CreateANewBoard";
+import SelectedBoard from "./components/SelectedBoard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Boards />
+        <SelectedBoard />
         <CreateANewBoard />
       </header>
     </div>
