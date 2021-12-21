21bcef288dab57d4a9e42f3586e251bf8075cca1
Edmine <edmine.de@gmail.com>
2021-12-21 14:46:34 -0500

    Created a test json file and stored an object name boards  which holds 3 different key value pairs, for id, title, and owner.

---
+++ inspiration-board-frontend/src/data/test.CreateANewBoard.js
@@ -0,0 +1,15 @@
+{"boards": [ 
+    { 
+        "id": "board.id", 
+        "title": "Happy Monday", 
+        "owner": "Min Delva"
+    }
+]
+}
+
+// "events": [
+//     {
+//         "person": "Adele Goldberg",
+//         "status": "In Smalltalk, everything happens somewhere else.",
+//         "timestamp": "2021-05-18T22:12:03Z"
+//     },
