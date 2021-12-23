299606289a11ce0d0e80bd61ecb40879d72b847a
Rhyannon <rhyannonjoy@gmail.com>
2021-12-23 11:36:14 -0700

    Added Bootstrap, implemented basic layout

--- inspiration-board-frontend/src/App.js
+++ inspiration-board-frontend/src/App.js
@@ -1,58 +1,55 @@
-// import "./App.css";
+import "./App.css";
 import Boards from "./components/Boards";
 import Card from "./components/Card";
 import CreateANewBoard from "./components/CreateANewBoard";
 import SelectedBoard from "./components/SelectedBoard";
-import {useState, useEffect} from 'react'; 
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-
-  const [boards, setBoards] = useState([{board_id: 127, owner: "Min.", title: "ATLA Quotes"}]
-  );
-  const [errorMessage, setErrorMessage] = useState('');
+  const [boards, setBoards] = useState([
+    { board_id: 127, owner: "Min.", title: "ATLA Quotes" },
+  ]);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const getBoards = () => {
-    axios.get('https://simons-ib-api.herokuapp.com/boards')
+    axios
+      .get("https://simons-ib-api.herokuapp.com/boards")
       .then((response) => {
         setBoards(response.data);
         // setErrorMessage('');
-      }).catch((error) => {
-        console.log(error)
+      })
+      .catch((error) => {
+        console.log(error);
         setErrorMessage(<section>{error.response.data.message}</section>);
       });
-      
   };
 
   useEffect(() => {
-    console.log("changes were made")
-}, [boards] ); 
+    console.log("changes were made");
+  }, [boards]);
 
-useEffect(() => {
-  getBoards()
-  console.log("changes were made")
-}, [] ); 
+  useEffect(() => {
+    getBoards();
+    console.log("changes were made");
+  }, []);
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <Boards boardsData = {boards} />
-=======
-    <header>
-      <div>
-        <Boards />
-      </div>
-      <div>
->>>>>>> 0f52b90bbfbf998608295f0f44c18abf55a1284b
-        <SelectedBoard />
-      </div>
-      <div>
-        <CreateANewBoard />
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <Boards boardsData={boards} />
+        </div>
+        <div className="col">
+          <SelectedBoard />
+        </div>
+        <div className="col">
+          <CreateANewBoard />
+        </div>
       </div>
       <div>
         <Card />
       </div>
-    </header>
+    </div>
   );
 }
 
