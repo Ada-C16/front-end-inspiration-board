a60f3003ec29f2aaae6fb558d6d5317c43213c3f
Andrea Yuson <andreasyuson@gmail.com>
2021-12-23 10:45:30 -0800

    Updates

---
+++ src/TitleBox.js
@@ -0,0 +1,17 @@
+import "./TitleBox.css";
+function TitleBox() {
+    return (
+    
+      <div class="index">
+      <div class="item-navbar"> Board </div> 
+      <div class= "item-navbar"> Selected Board</div> 
+      <div class="item-navbar"> Create a magical board</div>
+  
+    </div>
+    
+  
+    );
+  }
+  
+  export default TitleBox;
+
