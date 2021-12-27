0b782ac3190ec52232773ffef9d48238e28691d6
Jesse <jesseannepope@gmail.com>
2021-12-27 10:19:03 -0800

    added Card to forms component

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,10 +1,14 @@
-import React from "react"
-import Board from "./Board"
-import Card from "./Card"
-
+import React from "react";
+import Board from "./Board";
+import Card from "./Card";
 
 const Forms = () => {
-    return (<div><Board/></div>)
+  return (
+    <div>
+      <Board />
+      <Card />
+    </div>
+  );
 };
 
 export default Forms;
