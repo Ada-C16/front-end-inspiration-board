1db1ccfe3438051c56741952a5fa555b578b8171
Faith <fkauwe@gmail.com>
2021-12-27 10:40:41 -0800

    added comments to App.js and NewBoardForm.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ function App() {
   const addBoard = (boardInfo) => {
     console.log(boardInfo.title);
     console.log(boardInfo.ownersName);
+    // eventually this function will need to send a POST request through axios to the backend to
+    // actually create a new board
   };
 
   return (
     <div className="App">
       <header className="App-header"></header>
       <main>
+        {/* NewBoardForm is used and addBoard function is passed as prop named onAddBoard */}
         <NewBoardForm onAddBoard={addBoard} />
       </main>
     </div>
