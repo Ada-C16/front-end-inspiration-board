6a3b4bfa275321e7bb01162d07d09a13faec95f8
reidhowdy <87451286+reidhowdy@users.noreply.github.com>
2021-12-27 19:33:38 -0500

    Added form state, form field, and submit button to NewCardForm component. Not yet connected to app (no props passed yet).

--- front-end-inspiration-board/src/components/NewCardForm.js
+++ front-end-inspiration-board/src/components/NewCardForm.js
@@ -1,12 +1,43 @@
 import React from "react";
+import {useState } from 'react';
 
 
-const NewCardForm = () => {
+const NewCardForm = (props) => {
+    //create formField
+
+    const [cardFormField, setCardFormField] = useState({
+        cardMessage: ""
+    })
+
+    const onCardMessageChange = (event) => {
+        setCardFormField({
+            ...cardFormField,
+            cardMessage: event.target.value
+        })
+    }
+
+    const onCardFormSubmit = (event) => {
+        event.preventDefault();
+
+        //need to pass in this function as a prop
+        props.addNewCardCallback({
+            message: cardFormField.message
+        })
+    }
 
     return(
         <section className='new-card-form-block grid-block'>
             <h1> New Card Form</h1>
             This is the New Card Form.
+
+            <form onSubmit={onCardFormSubmit}>
+                <div>
+                    <label>New Card Message:</label>
+                    <input type="text" value={cardFormField.message} onChange={onCardMessageChange}></input>
+                </div>
+                <input type="submit" value="Submit"></input>
+            </form>
+
         </section>
     )
 }
