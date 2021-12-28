6463ebc3451de395b2b5b5b1b40be0499d8430df
Cassie <cassieroh@gmail.com>
2021-12-28 01:15:04 -0800

    Boardz auto-populates with board info

--- front-end-inspiration-board/src/components/Boardz.js
+++ front-end-inspiration-board/src/components/Boardz.js
@@ -1,53 +1,62 @@
 import React from "react";
-import Board from './Board';
-import './Boardz.css';
+import Board from "./Board";
+import "./Boardz.css";
 
 const Boardz = (props) => {
-
-    const onClearAllBoards = (event) =>{
-        const confirmBox = window.confirm(
-            'Are you sure?🥺'
-            )
-        if (confirmBox === true){
-            event.preventDefault();
-            // call inhereted function from props like 
-            props.deleteAllBoardsCallback();
-            console.log('delete all the boards!');
-        }
+  const onClearAllBoards = (event) => {
+    const confirmBox = window.confirm("Are you sure?🥺");
+    if (confirmBox === true) {
+      event.preventDefault();
+      // call inhereted function from props like
+      props.deleteAllBoardsCallback();
+      console.log("delete all the boards!");
     }
+  };
 
-    return(
-        <section className='boardz-block grid-block'>
-            <h1> Boardz List </h1>
-                <ul>
-                    <li>
-                        <Board selectNewBoard={props.updateCurrentBoardCallback}/>
-                    </li>
-                    <li>
-                        <Board />
-                    </li>
-                    <li>
-                        <Board />
-                    </li>
-                    <li>
-                        <Board />
-                    </li>
-                    <li>
-                        <Board />
-                    </li>
-                    <li>
-                        <Board />
-                    </li>
-                </ul>
-            <form>
-                <button 
-                    className="deleteAllBoardsButton" 
-                    onClick={onClearAllBoards}>
-                    clear all boards
-                </button>
-            </form>
-        </section>
-    )
-}
+  const boardsComponents = [];
+  const tempBoardList = [...props.boardList];
+  for (let oneBoard of tempBoardList) {
+    boardsComponents.push(
+      <li>
+        {console.log(oneBoard.id)}
+        <Board
+          selectNewBoard={props.updateCurrentBoardCallback}
+          boardInfo={oneBoard}
+        />
+      </li>
+    );
+  }
+  return (
+    <section className="boardz-block grid-block">
+      <h1> Boardz List </h1>
+      <ul>
+        {boardsComponents}
+        {/* {
+          <Board selectNewBoard={props.updateCurrentBoardCallback} />
+        </li>
+        <li>
+          <Board selectNewBoard={props.updateCurrentBoardCallback} />
+        </li>
+        <li>
+          <Board selectNewBoard={props.updateCurrentBoardCallback} />
+        </li>
+        <li>
+          <Board selectNewBoard={props.updateCurrentBoardCallback} />
+        </li>
+        <li>
+          <Board selectNewBoard={props.updateCurrentBoardCallback} />
+        </li>
+        <li>
+          <Board selectNewBoard={props.updateCurrentBoardCallback} />
+        </li>  */}
+      </ul>
+      <form>
+        <button className="deleteAllBoardsButton" onClick={onClearAllBoards}>
+          clear all boards
+        </button>
+      </form>
+    </section>
+  );
+};
 
 export default Boardz;
