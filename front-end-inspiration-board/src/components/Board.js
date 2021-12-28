00bb07eaf660b18a32d2611dc1a0a49becdb58fa
Cassie <cassieroh@gmail.com>
2021-12-28 01:16:42 -0800

    Board  displays title and passes id to app.js

--- front-end-inspiration-board/src/components/Board.js
+++ front-end-inspiration-board/src/components/Board.js
@@ -1,26 +1,23 @@
 import React from "react";
-import './Board.css';
+import "./Board.css";
 
 // This component should inheret the following from App.js as props:
-// -an object that contains the board title. 
-// -A callback function that will update state in App.js. 
+// -an object that contains the board title.
+// -A callback function that will update state in App.js.
 
 const Board = (props) => {
+  // This function is called when a user clicks on the name of a board
+  const handleSelectList = (event) => {
+    console.log("you clicked on the board you want to display!");
+    // invoke callback function from props here.This function should update currentBoard state in App.js.
+    props.selectNewBoard(props.boardInfo.id);
+  };
 
-    // This function is called when a user clicks on the name of a board
-    const handleSelectList = (event) => {
-        console.log("you clicked on the board you want to display!");
-        // invoke callback function from props here.This function should update currentBoard state in App.js. 
-        props.selectNewBoard();
-    }
-
-    return(
-        <section 
-            className="boardTab" onClick={handleSelectList}>
-                {/* prop.boardName or something similar here. but for now...⬇️*/}
-                A board named bitch 🌸
-        </section>
-    )
-}
+  return (
+    <section className="boardTab" onClick={handleSelectList}>
+      {props.boardInfo.title}🌸
+    </section>
+  );
+};
 
 export default Board;
