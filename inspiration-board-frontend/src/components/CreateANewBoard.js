6139e27adfe384683922ccd24a53806afa9722d1
Rhyannon <rhyannonjoy@gmail.com>
2021-12-28 21:44:20 -0700

    Added controlled form to CreateANewBoard.js

--- inspiration-board-frontend/src/components/CreateANewBoard.js
+++ inspiration-board-frontend/src/components/CreateANewBoard.js
@@ -12,16 +12,49 @@ import { useState } from "react";
 const CreateANewBoard = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
+  const [formFields, setFormFields] = useState({
+    title: "",
+    owner: "",
+  });
+
+  const onTitleChange = (event) => {
+    setFormFields({
+      ...formFields,
+      title: event.target.value,
+    });
+  };
+
+  const onOwnerChange = (event) => {
+    setFormFields({
+      ...formFields,
+      owner: event.target.value,
+    });
+  };
+
   return (
     <form action="" method="get" className="new-board-form">
       <h4>Temporary Header: Create A New Board</h4>
       <div className="board-title">
         <label htmlFor="title">Title </label>
-        <input type="text" id="title" required></input>
+        <input
+          type="text"
+          id="title"
+          title="Title"
+          value={formFields.title}
+          onChange={onTitleChange}
+          required
+        ></input>
       </div>
       <div className="board-owner">
         <label htmlFor="owner">Owner </label>
-        <input type="text" id="owner" required></input>
+        <input
+          type="text"
+          id="owner"
+          title="Owner"
+          value={formFields.owner}
+          onChange={onOwnerChange}
+          required
+        ></input>
       </div>
       <p>Preview: - </p>
       <SubmitQueryButton />
