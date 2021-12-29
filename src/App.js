f4ff167c115475561c56144c615368caf46d1b0a
karishma <karishmacarterjohnson@gmail.com>
2021-12-29 12:35:53 -0800

    removed testing data

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ import "./App.css";
 
 
 const App = () => {
-  /// TESTING: REMOVE WHEN LINKED CARDS ADDED TO DB
-  const sampleCards = [{id: 1, message: "words", likes_count:2}, {id: 2, message: "words", likes_count:4}, {id: 3, message: "words", likes_count:10}];
-  /// END TESTING
-
   const [currentBoard, setCurrentBoard] = useState(1);
   const [display, setDisplay] = useState(<Display id = "1" title="board" owner = "train-emoji" cards = {[]}/>);
   
@@ -21,11 +17,7 @@ const App = () => {
     setCurrentBoard(option);
     axios.get(`${process.env.REACT_APP_BACKEND_URL}/boards/${option}`)
       .then((response) => {
-        console.log(response.data)
         setDisplay(<Display title = {response.data.title} owner = {response.data.owner} cards = {response.data.cards}/>)
-        /// TESTING: REVERT PARAMETER WHEN CARDS WORK ^^^
-        // cards = {response.data.cards}
-        /// END TESTING
       })
   };
 
