c632dbc06da0bfb46432e1e1f4e2a647ee1af24c
Rhyannon <rhyannonjoy@gmail.com>
2021-12-30 14:40:11 -0700

    Hide New Board Form in CreateANewBoard now functions, requires yarn add v1.22.17 [1/4] Resolving packages... [2/4] Fetching packages... [3/4] Linking dependencies... [4/4] Building fresh packages... success Saved 1 new dependency. info Direct dependencies └─ react-collapse@5.1.1 info All dependencies └─ react-collapse@5.1.1 Done in 4.06s.

--- inspiration-board-frontend/src/components/CreateANewBoard.js
+++ inspiration-board-frontend/src/components/CreateANewBoard.js
@@ -1,16 +1,12 @@
 import SubmitButton from "./SubmitButton";
 import "./CreateANewBoard.css";
 import React from "react";
+import useCollapse from "react-collapsed";
 import { useState } from "react";
 
-// this is a CollapsibleForm component
-// this component needs to manage if the form
-// is collapsed or expanded
-// need isCollapsed, either true or false
-// need a way to update isCollapsed
-
 const CreateANewBoard = () => {
-  // const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isExpanded, setExpanded] = useState(true);
+  const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded });
 
   const [formFields, setFormFields] = useState({
     title: "",
@@ -34,38 +30,49 @@ const CreateANewBoard = () => {
   return (
     <form action="" method="get" className="new-board-form">
       <h4 className="create-new-board-header">Create A New Board</h4>
-      <div className="board-title">
-        <label htmlFor="title">Title </label>
-        <input
-          type="text"
-          id="title"
-          title="Title"
-          placeholder="give me a title!"
-          maxLength="40"
-          value={formFields.title}
-          onChange={onTitleChange}
-          required
-        ></input>
+      <div {...getCollapseProps()}>
+        <div className="board-title">
+          <label htmlFor="title">Title </label>
+          <input
+            type="text"
+            id="title"
+            title="Title"
+            placeholder="give me a title!"
+            maxLength="40"
+            value={formFields.title}
+            onChange={onTitleChange}
+            required
+          ></input>
+        </div>
+        <p></p>
+        <div className="board-owner">
+          <label htmlFor="owner">Owner </label>
+          <input
+            type="text"
+            id="owner"
+            title="Owner"
+            placeholder="who are you?"
+            maxLength="40"
+            value={formFields.owner}
+            onChange={onOwnerChange}
+            required
+          ></input>
+        </div>
+        <p></p>
+        <p>
+          Preview: {formFields.title} - {formFields.owner}
+        </p>
+        <p></p>
+        <SubmitButton />
       </div>
-      <div className="board-owner">
-        <label htmlFor="owner">Owner </label>
-        <input
-          type="text"
-          id="owner"
-          title="Owner"
-          placeholder="who are you?"
-          maxLength="40"
-          value={formFields.owner}
-          onChange={onOwnerChange}
-          required
-        ></input>
-      </div>
-      <p>
-        Preview: {formFields.title} - {formFields.owner}
-      </p>
-      <SubmitButton />
-      <button className="hide-new-board-button" type="button">
-        Hide New Board Form
+      <button
+        className="hide-new-board-button"
+        type="button"
+        {...getToggleProps({
+          onClick: () => setExpanded((prevExpanded) => !prevExpanded),
+        })}
+      >
+        {isExpanded ? "Hide New Board Form" : "Show New Board Form"}
       </button>
     </form>
   );
