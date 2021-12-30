e7e8b6cf9ce43854bfd955f1246b0da0dac47dbb
Faith <fkauwe@gmail.com>
2021-12-30 10:33:56 -0800

    added comments in app.js

--- src/App.js
+++ src/App.js
@@ -27,8 +27,10 @@ function App() {
         setBoards(result.data);
       });
   };
+  // boardId is a parameter for the function to access the specific board
   const getCards = (boardId) => {
     axios
+      // this axios endpoint mirrors the back-end endpoint for getting all cards for one board
       .get(`${process.env.REACT_APP_BACKEND_URL}/boards/${boardId}/cards`)
       .then((result) => {
         setCards(result.data);
@@ -43,6 +45,7 @@ function App() {
     // add newBoard to db and display updated board selections
     axios
       .post(`${process.env.REACT_APP_BACKEND_URL}/boards`, {
+        // this is the request body for the newBoard object
         title: boardInfo.title,
         author: boardInfo.ownersName,
       })
@@ -54,7 +57,10 @@ function App() {
   const handleAddCard = (message) => {
     // add newCard to db
     axios
+      // endpoint matches the endpoint in the backend
       .post(`${process.env.REACT_APP_BACKEND_URL}/cards`, {
+        // request body for newCard object, including boardID so the right board gets the card
+        // associated with it
         message,
         board_id: selectedBoard.id,
       })
@@ -80,6 +86,8 @@ function App() {
         {/* NewBoardForm is used and addBoard function is passed as prop named onAddBoard */}
         <NewBoardForm onAddBoard={handleAddBoard} />
         <BoardSelector boards={boards} onSelectBoard={updateCurrentBoard} />
+        {/* conditional logic to check for condition being satisfied to create NewCardForm
+         or CurrentBoard, CardList */}
         {selectedBoard && <NewCardForm onAddCard={handleAddCard} />}
         {selectedBoard && <CurrentBoard board={selectedBoard} />}
         {selectedBoard && (
