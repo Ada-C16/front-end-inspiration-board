b1db6d7a4aa46bedee36d163937b6284a6747b98
Faith <fkauwe@gmail.com>
2021-12-30 22:01:49 -0800

    finished functionality for +1 button and delete button, both testing correctly

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,18 +1,24 @@
 import PropTypes from "prop-types";
-import Card from "./Card"
+import Card from "./Card";
 // "card" and {card} generally refer to the data being passed through props
 // while Card is the component and component file
-const CardList = ({cards,onIncreaseLikes})=>{
-    return (
-        <div> 
-            {/* using map function to iterate through list of cards and create a new
+const CardList = ({ cards, onIncreaseLikes, onDeleteOneCard }) => {
+  return (
+    <div>
+      {/* using map function to iterate through list of cards and create a new
              html Card element for each card  */}
-            {cards.map((card)=> {
-                return <Card key={card.id} card={card}
-                onIncreaseLikes={onIncreaseLikes}/>
-            })}
-        </div>
-    )
-}
+      {cards.map((card) => {
+        return (
+          <Card
+            key={card.id}
+            card={card}
+            onIncreaseLikes={onIncreaseLikes}
+            onDeleteOneCard={onDeleteOneCard}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default CardList;
