5975a4d5aa61a19d50fda792da14e967dec353e5
karishma <karishmacarterjohnson@gmail.com>
2022-01-01 11:29:33 -0800

    lifted Display state to App

--- src/App.js
+++ src/App.js
@@ -9,7 +9,18 @@ import train from './images/train-gif.gif'
 const App = () => {
   const [currentBoard, setCurrentBoard] = useState(1);
   const [display, setDisplay] = useState(<Display id = "1" title="board" owner = "train-emoji" cards = {[]}/>);
+  const [cards, setCards] = useState([]);
+  const [boardInfo, setBoardInfo] = useState({title:"",owner:""});
   
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/boards/1`) // what if 1 doesnt exist? catch by going to next avail num
+      .then((response) => {
+        setBoardInfo({title:response.data.title, owner:response.data.owner})
+        setCards(response.data.cards)
+      })
+  }, [])
+
+  // COMPONENTS 
   const onBoardSelect = (e) => {
     const index = e.target.selectedIndex;
     const el = e.target.childNodes[index];
@@ -18,19 +29,63 @@ const App = () => {
     setCurrentBoard(option);
     axios.get(`${process.env.REACT_APP_BACKEND_URL}/boards/${option}`)
       .then((response) => {
-        setDisplay(<Display title = {response.data.title} owner = {response.data.owner} cards = {response.data.cards}/>)
+        setBoardInfo({title:response.data.title, owner:response.data.owner})
+        setCards(response.data.cards)
       })
   };
 
   useEffect(() => {
-    // // get first child element getAttribute(id) from onBoardSelect
-    // const firstBoard = document.getElementById('board-select-menu');
-    // console.log(firstBoard);
-    axios.get(`${process.env.REACT_APP_BACKEND_URL}/boards/1`) // what if 1 doesnt exist? catch by going to next avail num
-      .then((response) => {
-        setDisplay(<Display title = {response.data.title} owner = {response.data.owner} cards = {response.data.cards}/>)
-      })
-  }, [])
+    const likeCard = (e) => {
+      const id = e.target.parentNode.parentNode.getAttribute("id");
+      axios
+        .put(`${process.env.REACT_APP_BACKEND_URL}/cards/${id}/like`)
+        .then(() => {
+          const newCards = cards.map((card) => {
+            if (card.id === parseInt(id)) {
+              card.likes_count += 1;
+            }
+            return card;
+          });
+          setCards(newCards);
+        })
+        .catch(() => {}); // maybe just have it superficially update the number // or do nothing
+    };
+
+    const deleteCard = (e) => {
+      const id = e.target.parentNode.parentNode.getAttribute("id");
+      axios
+        .delete(`${process.env.REACT_APP_BACKEND_URL}/cards/${id}`)
+        .then(() => {
+          const newCards = [];
+          cards.forEach((card) => {
+            if (card.id !== parseInt(id)) {
+              newCards.push(card);
+            }
+          });
+          setCards(newCards); 
+        });
+    };
+
+    if (cards) {
+      const cardComponentList = cards.map((card) => {
+        return (
+          <div className="card-view" key={card.id} id={card.id}>
+            <div class="card-message">{card.message}</div>
+            <div className="card-buttons">
+              <p class="likes-count">&#9734;{card.likes_count}</p>
+              <button className="like" onClick={likeCard}>
+                +1
+              </button>
+              <button className="delete" onClick={deleteCard}>
+                delete
+              </button>
+            </div>
+          </div>
+        );
+      });
+      setDisplay(<Display title ={boardInfo.title} owner = {boardInfo.owner} cards = {cards} cardsComponents = {cardComponentList}/>)
+    }
+  }, [cards]);
 
   return (
     <div className="App">
--- src/components/Display.js
+++ src/components/Display.js
@@ -3,65 +3,7 @@ import axios from "axios";
 import "./Display.css";
 
 const Display = (props) => {
-  const [cards, setCards] = useState([]);
-  const [cardsComponents, setCardsComponents] = useState([]);
 
-  useEffect(() => {
-    setCards(props.cards);
-  }, [props]);
-
-  useEffect(() => {
-    const likeCard = (e) => {
-      const id = e.target.parentNode.parentNode.getAttribute("id");
-      axios
-        .put(`${process.env.REACT_APP_BACKEND_URL}/cards/${id}/like`)
-        .then(() => {
-          const newCards = cards.map((card) => {
-            if (card.id === parseInt(id)) {
-              card.likes_count += 1;
-            }
-            return card;
-          });
-          setCards(newCards);
-        })
-        .catch(() => {}); // maybe just have it superficially update the number // or do nothing
-    };
-
-    const deleteCard = (e) => {
-      const id = e.target.parentNode.parentNode.getAttribute("id");
-      axios
-        .delete(`${process.env.REACT_APP_BACKEND_URL}/cards/${id}`)
-        .then(() => {
-          const newCards = [];
-          cards.forEach((card) => {
-            if (card.id !== parseInt(id)) {
-              newCards.push(card);
-            }
-          });
-          setCards(newCards); 
-        });
-    };
-
-    if (cards) {
-      const cardComponentList = cards.map((card) => {
-        return (
-          <div className="card-view" key={card.id} id={card.id}>
-            <div class="card-message">{card.message}</div>
-            <div className="card-buttons">
-              <p class="likes-count">&#9734;{card.likes_count}</p>
-              <button className="like" onClick={likeCard}>
-                +1
-              </button>
-              <button className="delete" onClick={deleteCard}>
-                delete
-              </button>
-            </div>
-          </div>
-        );
-      });
-      setCardsComponents(cardComponentList);
-    }
-  }, [cards]);
 
   return (
     <div className="display-component">
@@ -70,7 +12,7 @@ const Display = (props) => {
           "{props.title}" by {props.owner}
         </span>
       </h2>
-      <div className="cards-display">{cardsComponents}</div>
+      <div className="cards-display">{props.cardsComponents}</div>
     </div>
   );
 };
