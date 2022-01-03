2b6ff8a2aa87ee6babc7a1fb91a47632144ee055
Katie <diazkatie@gmail.com>
2022-01-03 11:37:43 -0800

    Created Board & Card components with sample message

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <p>Insurt message here</p> 
+      <span>hearts</span>
+      <span>+1</span>
+      <span>delete</span>
+
+    </div> 
+  )
+}
+
+
+export default Card;
