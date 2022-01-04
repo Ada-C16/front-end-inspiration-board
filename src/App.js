d2f3e68665958680d7eed0138d54bd0b3393601c
Mac <mackenziespeyton@gmail.com>
2022-01-04 12:52:09 -0800

    renders boards and cards from database

--- src/App.js
+++ src/App.js
@@ -1,146 +1,189 @@
-import './App.css';
-import BoardList from './components/BoardList';
-import BoardForm from './components/BoardForm';
-import CardList from './components/CardList';
-import React, { useState } from 'react';
-import CardForm from './components/CardForm';
-import axios from 'axios';
+import "./App.css";
+import BoardList from "./components/BoardList";
+import BoardForm from "./components/BoardForm";
+import CardList from "./components/CardList";
+import React, { useState, useEffect } from "react";
+import CardForm from "./components/CardForm";
+import axios from "axios";
 
-const URL = 'https://inspirandwich-board.herokuapp.com/'
+const URL = "https://inspirandwich-board.herokuapp.com/";
 
-
-const CARDS = [{id:1, message:"Hi hello", likes_count: 1}, 
-                {id:2, message:"Howdy", likes_count: 1},
-                {id:3, message:"Yoohoo", likes_count: 1}]
-
-const BOARDS = [{id:1, owner:"owner a", title: "Title 1"}, 
-                {id:2, owner:"owner b", title: "Title 2"}, 
-                {id:3, owner:"owner c", title: "Title 3"}]
-
+// const CARDS = [
+//   { id: 1, message: "Hi hello", likes_count: 1 },
+//   { id: 2, message: "Howdy", likes_count: 1 },
+//   { id: 3, message: "Yoohoo", likes_count: 1 },
+// ];
 
 function App() {
-
   // ---------------------------------------------------------------
-  // --------------------- ESTABLISHING STATES --------------------- 
+  // --------------------- ESTABLISHING STATES ---------------------
   // ---------------------------------------------------------------
   // This sets up the state for the 'Board Selected' value
-  const [selectedBoard, setSelectedBoard] = useState('');
-  
-  // This sets up the state for the boards displayed in 'Boards'
-  const [boards, setBoards] = useState(BOARDS);
-  
-  // This sets up the state for the cards displayed in 'Cards for Board'
-  const [cards, setCards] = useState(CARDS)
+  const [selectedBoard, setSelectedBoard] = useState("");
 
+  // This sets up the state for the boards displayed in 'Boards'
+  const [boards, setBoards] = useState([]);
+
+  // This sets up the state for the cards displayed in 'Cards for Board'
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    generateBoards();
+  }, []);
+
+  const generateBoards = () => {
+    let boards = [];
+    axios
+      .get(`${URL}/boards`)
+      .then((response) => {
+        boards = response.data.map((board) => {
+          return {
+            id: board.id,
+            owner: board.owner,
+            title: board.title,
+          };
+        });
+        setBoards(boards);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   // ---------------------------------------------------------------
-  // ------------------- SETTING STATES FUNCTIONS------------------- 
+  // ------------------- SETTING STATES FUNCTIONS-------------------
   // ---------------------------------------------------------------
   // This sets state for the 'Board Selected' value when a board is clicked
-  const selectBoard = (id) => { 
-    for (let board of boards) { // I think this should be boards not BOARDS? 
-      if (id===board.id) {
-        setSelectedBoard(board)
-      }}}
+  const selectBoard = (id) => {
+    for (let board of boards) {
+      // I think this should be boards not BOARDS?
+      if (id === board.id) {
+        setSelectedBoard(board);
+        axios
+          .get(`${URL}/boards/${id}`)
+          .then((response) => {
+            const cards = response.data.map((card) => {
+              return {
+                id: card.id,
+                message: card.message,
+                likes_count: card.likes_count,
+              };
+            });
+            setCards(cards);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    }
+  };
 
   // This sets the state for the 'Boards', this changes when a new board is added, we will also need to add functionality to delete all?
   const addBoard = (boardInfo) => {
-    
     axios
-    .post(`${URL}/boards`,{
-      "title": boardInfo.title,
-      "owner": boardInfo.owner,
-    })
-    
-    .then((response)=>{
-      
-      const newBoards = [...boards]
-      const newBoard ={id:response.data.id,
-        title:boardInfo.title,
+      .post(`${URL}/boards`, {
+        title: boardInfo.title,
         owner: boardInfo.owner,
-      }
-        
-      
-      boardInfo.id = response.data.id;
-      newBoards.push(newBoard);
-      setBoards(newBoards)
+      })
 
-    })
-    
-    .catch((error)=>{
-      console.log(error.response.data);
-    });
-  }
+      .then((response) => {
+        const newBoards = [...boards];
+        const newBoard = {
+          id: response.data.id,
+          title: boardInfo.title,
+          owner: boardInfo.owner,
+        };
+
+        boardInfo.id = response.data.id;
+        newBoards.push(newBoard);
+        setBoards(newBoards);
+      })
+
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  };
 
   // This sets the state for the 'Cards', this changes when a card is liked
-  const updateCardLikes = (id) => {
-      const updatedCards = [...cards]
-      for (let card of updatedCards) {
-        if (id === card.card_id) {
-          card.likes_count++
-          // axios.put(`https://inspirandwich-board.herokuapp.com/boards/${card.board_id}}/${id}`)
+  const updateCardLikes = (id, likes_count) => {
+    axios
+      .put(`${URL}/boards/${selectedBoard.id}/${id}`, {
+        likes_count: likes_count,
+      })
+      .then((response) => {
+        const updatedCards = [...cards];
+        for (let card of updatedCards) {
+          if (id === card.card_id) {
+            card.likes_count = response.data.likes_count;
+          }
         }
-      }
-      setCards(updatedCards)
-    }
+        console.log(`UPDATEDDD CARDDDS`, updatedCards);
+        setCards(updatedCards);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  };
 
   // This sets the states for 'Cards', this changes when a card is deleted from the deck
   const deleteCard = (id) => {
-    const updatedCards = [...cards]
+    const updatedCards = [...cards];
     const newCards = updatedCards.filter((card) => {
       if (card.id !== id) {
-        return card
+        return card;
       }
-    })
-    setCards(newCards)
-    }
-  
+    });
+    setCards(newCards);
+  };
+
   // This sets the states for 'Cards', this changes when a card is added to the deck
   const addCard = (cardInfo) => {
-    // const num = 4 // We can delete this when we link up to database since the id will be generated through that. 
+    // const num = 4 // We can delete this when we link up to database since the id will be generated through that.
     // const newCard = {id: num, message: cardInfo.message, likes_count: cardInfo.likes_count}
     // creating axios post method
     // axios.post(`https://inspirandwich-board.herokuapp.com/boards/${}/cards`)
     // const updatedCards = [...cards]
     // updatedCards.push(newCard)
     // setCards(updatedCards)
-  }
+  };
 
   return (
     <div className="App">
       <h1>Inspiration Board</h1>
-      
-      <section className='boards_container'>
+
+      <section className="boards_container">
         <section>
           <h2>Boards</h2>
-          <BoardList boards={boards} onClickBoard={selectBoard}/>
+          <BoardList boards={boards} onClickBoard={selectBoard} />
         </section>
-        
+
         <section>
           <h2>Board Selected</h2>
           <ul>
-              <li>
-                {selectedBoard.title}
-              </li>
+            <li>{selectedBoard.title}</li>
           </ul>
         </section>
 
         <section>
           <h2>Create a New Board</h2>
-          <BoardForm onClickAddBoard={addBoard}/>
+          <BoardForm onClickAddBoard={addBoard} />
         </section>
       </section>
-      
-      <section className='cards_container'>
+
+      <section className="cards_container">
         <section>
           <h2>Cards for Board</h2>
-          <CardList cards={cards} onClickLike={updateCardLikes} onClickDelete={deleteCard}/>
+          <CardList
+            cards={cards}
+            onClickLike={updateCardLikes}
+            onClickDelete={deleteCard}
+          />
         </section>
-        
+
         <section>
-            <h2>Create A New Card</h2>
-            <CardForm onClickAddCard={addCard} />
-          </section>
-      </section>    
+          <h2>Create A New Card</h2>
+          <CardForm onClickAddCard={addCard} />
+        </section>
+      </section>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,36 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import './Card.css';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./Card.css";
 
-const Card = ({ id, message, likes_count, onClickLike, onClickDelete}) => {
-    return (
+const Card = ({ id, message, likes_count, onClickLike, onClickDelete }) => {
+  return (
     <li>
-        Card {id}: {message} Likes: {likes_count} <button onClick={()=>{onClickLike(id)}}> +1 </button> <button onClick={()=>{onClickDelete(id)}}>Delete</button>
+      Card {id}: {message} Likes: {likes_count}{" "}
+      <button
+        onClick={() => {
+          onClickLike(id, likes_count);
+        }}
+      >
+        {" "}
+        +1{" "}
+      </button>{" "}
+      <button
+        onClick={() => {
+          onClickDelete(id);
+        }}
+      >
+        Delete
+      </button>
     </li>
-    );
+  );
 };
 
 Card.propTypes = {
-    id: PropTypes.number.isRequired,
-    message: PropTypes.string.isRequired,
-    likes_count: PropTypes.number.isRequired, 
-    onClickLike: PropTypes.func.isRequired, 
-    onClickDelete: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired,
+  message: PropTypes.string.isRequired,
+  likes_count: PropTypes.number.isRequired,
+  onClickLike: PropTypes.func.isRequired,
+  onClickDelete: PropTypes.func.isRequired,
 };
 
 export default Card;
