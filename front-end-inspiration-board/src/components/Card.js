4c42fd04559587c96b6b27747e52b29b7330dbf6
reidhowdy <87451286+reidhowdy@users.noreply.github.com>
2022-01-04 20:35:26 -0800

    Tinkered to try to fix the infinite loop. No success. Not sure why sortAllCards is running over and over

--- front-end-inspiration-board/src/components/Card.js
+++ front-end-inspiration-board/src/components/Card.js
@@ -11,7 +11,6 @@ const Card = (props) => {
     // props.updateCardDisplayCallback(props.cardList, props.currentBoard)
 
 
-<<<<<<< HEAD
     // const currentCards = [...props.cardList];
     // for (let card of currentCards) {
     //   if (props.id === card.card_id) {
@@ -20,14 +19,13 @@ const Card = (props) => {
   }
   
 
-=======
     const currentCards = [...props.cardList];
     for (let card of currentCards) {
       if (props.id === card.card_id) {
         setCardLikeCount(card.cardLikes + 1)
         props.addLikeCallback(card.card_id)
       }
-  }}
+  }
   
   // this function is for debugging purposes
   const logCardInfo = () => {
@@ -40,7 +38,6 @@ const Card = (props) => {
     });
   }
   
->>>>>>> c05ac281116e1e43b3cd307a79a8158de5f2acf8
 
   //deletes card from state AND from backend seperately (to ensure frontend updates w/o waiting for request to backend)
   const handleDeleteCard = () => {
