1af13fbe52e95f32ba375b516077afbbce404df3
Lizet Tovar <lizett503@gmail.com>
2022-01-04 20:05:37 -0800

    Co-authored-by: Jacy Yang <jacyyang04@users.noreply.github.com>

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,11 +1,29 @@
 import PropTypes from "prop-types";
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import Card from "./Card";
 import "./CardList.css";
 
-// CardList will render cards
+// like route --->  /cards/<card_id>/like
+const URL = process.env.REACT_APP_BACKEND_URL;
 
 const CardList = () => {
+  // state for cards
+  const [cards, setCards] = useState([]);
+  const getCards = () => {
+    axios.get(`${URL}/board_id/cards`).then((response) => {
+      console.log(response.data);
+      const newCards = response.data.map((card) => {
+        return {
+          message: card.message,
+          likes_count: card.likes_count,
+          // ?board or card id?
+        };
+      });
+    });
+  };
+
   return (
     <div className="card-container">
       <Card />
--- src/components/NewCard.js
+++ src/components/NewCard.js
@@ -1,20 +1,43 @@
-import PropTypes from "prop-types";
 import React from "react";
+import { useState } from "react";
 import "./NewCard.css";
 
 const NewCard = () => {
+  const emptyForm = {
+    message: "",
+  };
+
+  const [formFields, setFormFields] = useState(emptyForm);
+
+  const addMessage = (e) => {
+    setFormFields({
+      message: e.target.value,
+    });
+    document.getElementById("cardPreview").textContent = e.target.value;
+  };
+  const messageSubmit = (e) => {
+    e.preventDefault();
+    console.log(formFields);
+    setFormFields(emptyForm);
+  };
+
   return (
-    <div>
-      <div>Make a New Card!</div>
+    <form onSubmit={messageSubmit}>
       <div>
-        <label>
-          Message:
-          <input type="text" />
-        </label>
+        <div>Make a New Card!</div>
+        <div>
+          <label>Message:</label>
+          <input
+            type="text"
+            name="message"
+            onChange={addMessage}
+            value={formFields.message}
+          ></input>
+        </div>
+        <span>Preview</span>
+        <button>Submit</button>
       </div>
-      <span>Preview</span>
-      <button>Submit</button>
-    </div>
+    </form>
   );
 };
 
