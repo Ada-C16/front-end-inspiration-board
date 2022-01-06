d0f813ac56364c8182fe878e932150cbed1600b1
sarahstandish <z.sisyphusrocks@gmail.com>
2022-01-05 17:11:22 -0800

    display board id

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,7 +3,7 @@ import React from "react";
 const Board = ({ board, getCards }) => {
   return (
     <li>
-      {board.title} by {board.owner}
+      {board.title} by {board.owner}. Board ID: {board.board_id}
       <button onClick={() => getCards(board.board_id)}>See cards</button>
     </li>
   );
